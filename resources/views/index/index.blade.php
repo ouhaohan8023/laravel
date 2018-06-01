34cd4ae5c8589a33012b3b6314203234238e6361
137454534@qq.com <mushroom1117>
2018-06-01 14:00:31 +0800

    ico

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -26,6 +26,14 @@
 	<link rel="dns-prefetch" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
+	{{--ico--}}
+	<link rel="apple-touch-icon" sizes="180x180" href="/ico/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/ico/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/ico/favicon-16x16.png">
+	<link rel="manifest" href="/ico/site.webmanifest">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
+
 	<!-- Styles -->
 	{{--<link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
 	<style>
