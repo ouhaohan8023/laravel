e563f58b0ff749eaa35821a26e5bde7c1ba9dae0
137454534@qq.com <mushroom1117>
2018-06-01 16:45:29 +0800

    Logo

--- config/app.php
+++ config/app.php
@@ -219,4 +219,16 @@ return [
       'MarkdownEditor' => Chenhua\MarkdownEditor\Facades\MarkdownEditor::class,
     ],
 
+  /*
+   |--------------------------------------------------------------------------
+   | Application Name
+   |--------------------------------------------------------------------------
+   |
+   | This value is the name of your application. This value is used when the
+   | framework needs to place the application's name in a notification or
+   | any other location as required by the application or its packages.
+   |
+   */
+
+    'logo' => env('APP_LOGO'),
 ];
