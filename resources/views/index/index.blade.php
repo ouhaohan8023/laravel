a773d93a8ab3a3a68f17ef4988d00b837ca5b607
137454534@qq.com <mushroom1117>
2018-06-04 11:08:06 +0800

    友盟3

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -91,7 +91,7 @@
 	</script>
 
 	{{--友盟--}}
-	<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1273865734'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s13.cnzz.com/z_stat.php%3Fid%3D1273865734%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>
+	{{--<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1273865734'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s13.cnzz.com/z_stat.php%3Fid%3D1273865734%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));</script>--}}
 </head>
 <body>
 <div id="app">
@@ -121,6 +121,9 @@
 			</el-container>
 			<el-footer>Footer</el-footer>
 		</el-container>
+		<div style="display: none">
+			<script src="https://s13.cnzz.com/z_stat.php?id=1273865734&web_id=1273865734" language="JavaScript"></script>
+		</div>
 		<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
 			@csrf
 		</form>
@@ -149,9 +152,6 @@
 	}
 
 
-</script>
-<script>
-    $('#cnzz_stat_icon_1273865734').css('display','none')
 </script>
 </body>
 </html>
