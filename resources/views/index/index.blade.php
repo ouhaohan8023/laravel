5514d88fe726b43bf905f5bf7889ecab2b3b1dae
ohh <mrmercury@foxmail.com>
2018-06-17 23:20:51 +0800

    禁止缩放

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=0;">
 	<!--默认使用极速内核：针对国内浏览器产商-->
 	<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
 	<meta name="description"  content="Ohh的博客,一个叙述生活的地方" />
@@ -200,7 +200,7 @@
 	//	console.log(document.body.clientWidth)
 	ScreenWidth = document.body.clientWidth;
 	if(ScreenWidth <= 764){
-		isCollapse = true;
+//		isCollapse = true;
 		ShowTitle = true;
 	}
 //	console.log(document.documentElement.clientHeight)
